d267e2e260250ee0ba92e3ccb56eec4d4c5738ce
bariflor <barihuamachuco@gmail.com>
2017-08-10 15:26:05 -0500

    desafio-5

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var contenedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var verde = document.getElementById("verde");
+var amarillo = document.getElementById("amarillo");
+contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.Width = "600px";
+contenedor.style.padding = "50px";
+
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
